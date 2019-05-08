ddd82b1abc7ee87a2e018d5dfb2ce222d6f9e421
Vikas Dhiman <vdhiman@ucsd.edu>
2019-05-08 00:05:55 -0700

    XAUTH is not a directory but a file

--- project-docker.d/setup.sh
+++ project-docker.d/setup.sh
@@ -5,7 +5,7 @@ command -v docker-compose || {
     echo "Install docker-compose" \
     && sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 }
-XAUTH=$(mktemp -d --suffix=.xauth)
+XAUTH=$(mktemp --suffix=.xauth)
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-docker-compose -v $XAUTH -e XAUTHORITY=$XAUTH run terminal
+docker-compose -v $XAUTH -e XAUTHORITY=$XAUTH run
 
